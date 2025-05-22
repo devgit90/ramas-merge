96774cb5d8e36a17b0a7fd46853b58a68719a15c
Ross Code <zamr616@outlook.es>
2025-05-22 09:26:36 -0600

    Se trabaja pandas en python

---
+++ ex_ps.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+data = np.array([[1,2],[2,7],[3,6]])
+
+df = pd.DataFrame(data)
+
+print(df)
---
+++ texto.py
@@ -0,0 +1,11 @@
+import os
+
+print(os.getcwd())
+
+os.makedirs("DesdeVSC")
+
+print("Hola Mundo desde VSC")
+
+algo ="Rosa Zarco"
+
+print(f, 'Hola {algo}')
